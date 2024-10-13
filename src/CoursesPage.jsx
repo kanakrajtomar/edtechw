8fb2aacc2575b5c421c945ff26c9e2ec01137e46
Kanak Raj Tomar <112727321+kanakrajtomar@users.noreply.github.com>
2024-10-13 17:10:46 +0530

    CoursesPage.jsx

--- src/CoursesPage.jsx
+++ src/CoursesPage.jsx
@@ -150,7 +150,7 @@ const CoursesPage/*: React.FC*/ = () => {
                 </div>
                 <div className="footer-image">
                     <img
-                        src="/src\assets\11453335-removebg-preview.png?height=100&width=200"
+                        src="/11453335-removebg-preview.png?height=100&width=200"
                         height="100"
                         width="200"
                         alt="FAQ illustration"
